efbe198e31872dfb14b802e96f382c0b3077fa71
Brian Barto <bartobrian@gmail.com>
2016-07-26 15:48:21 -0400

    Implementation of a "last seen" table containing communication timestampes for every connected socket.
    	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -20,13 +20,21 @@
 #define DEFAULT_HOST        NULL
 #define PERIODIC_SECONDS    5
 
+struct lastseen {
+	int socket;
+	time_t last_time;
+	struct lastseen *next;
+};
+
 // Function prototypes
+void set_last_seen(int);
 void error(const char *);
 void handle_sigint(int);
 void cleanup(void);
 
 // Globals
 int mainsockfd;
+struct lastseen *ls_start = NULL;
 
 /*
  * int main(int, char *)
@@ -106,6 +114,9 @@ int main(int argc, char *argv[]) {
 		if (r > 0) {
 			for (i = 0; i < FD_SETSIZE; ++i) {
 				if (FD_ISSET (i, &read_fd_set)) {
+					
+					// Update lastseen table
+					set_last_seen(i);
 
 					// Handle new server connections here in main.c
 					if (i == mainsockfd && comp_type() == SERVER) {
@@ -123,6 +134,9 @@ int main(int argc, char *argv[]) {
 						
 						// Adding new connection to fd set
 						FD_SET(newsockfd, &active_fd_set);
+						
+						// Update lastseen table
+						set_last_seen(newsockfd);
 						
 						// TODO - handshake here?
 					
@@ -188,6 +202,59 @@ int main(int argc, char *argv[]) {
 	}
 
 	return 0;
+}
+
+/*
+ * void update_last_seen(int)
+ *
+ * DESCR:
+ * Updates a table of all sockets with the last time communication
+ * was received for a given socket number.
+ *
+ * ARGS:
+ * int socket - socket to be updated
+ *
+ */
+ // TODO - remove printf statements in this function
+void set_last_seen(int socket) {
+	struct lastseen *ls_pointer;
+	
+	// If list is empty, create first record.
+	if (ls_start == NULL) {
+		ls_start = malloc(sizeof(struct lastseen));
+		ls_start->socket = socket;
+		ls_start->last_time = time(NULL);
+		ls_start->next = NULL;
+		//printf("updated lastseen for socket %i (first entry)\n", ls_start->socket);
+		return;
+	}
+	
+	// Loop over list and either update existing record of create new
+	ls_pointer = ls_start;
+	while (true) {
+		
+		//Update record if exists
+		if (ls_pointer->socket == socket) {
+			ls_pointer->last_time = time(NULL);
+			//printf("updated lastseen for socket %i (existing entry)\n", ls_pointer->socket);
+			break;
+		}
+		
+		// Append new record for socket
+		if (ls_pointer->next == NULL) {
+			ls_pointer->next = malloc(sizeof(struct lastseen));
+			ls_pointer = ls_pointer->next;
+			ls_pointer->socket = socket;
+			ls_pointer->last_time = time(NULL);
+			ls_pointer->next = NULL;
+			//printf("updated lastseen for socket %i (new entry)\n", ls_pointer->socket);
+			break;
+		}
+
+		// Get next record
+		//printf("Slipping record for socket %i\n", ls_pointer->socket);
+		ls_pointer = ls_pointer->next;
+	}
 }
 
 /*
