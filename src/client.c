8cd5fa33b34f2ace59ffc903c8c39b782b6fd533
Brian Barto <bartobrian@gmail.com>
2016-08-25 13:08:23 -0400

    Initial server/client dialog for connecting and getting game info.
    	modified:   src/client.c
    	modified:   src/server.c

--- src/client.c
+++ src/client.c
@@ -3,6 +3,7 @@
 // This program is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License. See LICENSE for more details.
 
+#include <stdio.h>
 #include "modules/network.h"
 #include "modules/command.h"
 #include "modules/periodic.h"
@@ -13,8 +14,52 @@ COMMAND_RETURN command_helo(COMMAND_ARGS) {
 	(void)socket;
 	(void)payload;
 	
-	// Use helo command to display an initial message to the client
-	// or to initiate a process.
+	network_write(mainsocket_get(), "info");
+	
+	return 0;
+}
+
+COMMAND_RETURN command_info(COMMAND_ARGS) {
+	char o;
+	int loop;
+
+	(void)socket;
+	(void)payload;
+	
+	printf("Table Info\n%s\n\n", payload);
+	
+	printf("Menu\n");
+	printf("1 - Join\n");
+	printf("2 - Leave\n\n");
+	
+	loop = 1;
+
+	while (loop) {
+		printf("Choose: ");
+		o = getchar();
+		
+		switch(o) {
+			case '1':
+				network_write(mainsocket_get(), "join");
+				loop = 0;
+				break;
+			case '2':
+				network_write(mainsocket_get(), "quit");
+				loop = 0;
+				break;
+			default:
+				printf("Invalid option.\n");
+		}
+	}
+	
+	return 0;
+}
+
+COMMAND_RETURN command_aval(COMMAND_ARGS) {
+	(void)socket;
+	(void)payload;
+	
+	printf("%s\n", payload);
 	
 	return 0;
 }
@@ -29,5 +74,7 @@ PERIODIC_RETURN send_heartbeat(PERIODIC_ARGS) {
 
 void load_functions(void) {
 	command_add("helo", &command_helo);
+	command_add("info", &command_info);
+	command_add("aval", &command_aval);
 	periodic_add(&send_heartbeat);
 }
--- src/server.c
+++ src/server.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include "modules/network.h"
 #include "modules/periodic.h"
 #include "modules/command.h"
 #include "modules/socketlist.h"
@@ -15,6 +16,25 @@
 /*
  * Define functions here
  */
+ 
+COMMAND_RETURN command_info(COMMAND_ARGS) {
+	(void)socket;
+	(void)payload;
+
+	network_write(socket, "info" "Info about table rules here.");
+	
+	return 0;
+}
+
+COMMAND_RETURN command_join(COMMAND_ARGS) {
+	(void)socket;
+	(void)payload;
+
+	network_write(socket, "aval" "Info about open seats.");
+	
+	return 0;
+}
+
 COMMAND_RETURN command_quit(COMMAND_ARGS) {
 	
 	// Suppress "unused parameter" warning for payload
@@ -56,6 +76,8 @@ PERIODIC_RETURN periodic(PERIODIC_ARGS) {
  */
 void load_functions(void) {
 	command_add("beat", &command_beat);
+	command_add("info", &command_info);
+	command_add("join", &command_join);
 	command_add("quit", &command_quit);
 	periodic_add(&periodic);
 }
