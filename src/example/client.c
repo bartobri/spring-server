4e71957311b6368f11c78738bb53168f5fac3c9e
Brian Barto <bartobrian@gmail.com>
2016-08-25 17:00:30 -0400

    Move blackjack files to example folder
    	renamed:    blackjack.h -> example/blackjack.h
    	new file:   example/client.c
    	new file:   example/server.c

---
+++ src/example/client.c
@@ -0,0 +1,80 @@
+// Copyright (c) 2016 Brian Barto
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License. See LICENSE for more details.
+
+#include <stdio.h>
+#include "modules/network.h"
+#include "modules/command.h"
+#include "modules/periodic.h"
+#include "modules/socketlist.h"
+#include "modules/mainsocket.h"
+
+COMMAND_RETURN command_helo(COMMAND_ARGS) {
+	(void)socket;
+	(void)payload;
+	
+	network_write(mainsocket_get(), "info");
+	
+	return 0;
+}
+
+COMMAND_RETURN command_info(COMMAND_ARGS) {
+	char o;
+	int loop;
+
+	(void)socket;
+	(void)payload;
+	
+	printf("Table Info\n%s\n\n", payload);
+	
+	printf("Menu\n");
+	printf("1 - Join\n");
+	printf("2 - Leave\n\n");
+	
+	loop = 1;
+
+	while (loop) {
+		printf("Choose: ");
+		o = getchar();
+		
+		switch(o) {
+			case '1':
+				network_write(mainsocket_get(), "join");
+				loop = 0;
+				break;
+			case '2':
+				network_write(mainsocket_get(), "quit");
+				loop = 0;
+				break;
+			default:
+				printf("Invalid option.\n");
+		}
+	}
+	
+	return 0;
+}
+
+COMMAND_RETURN command_aval(COMMAND_ARGS) {
+	(void)socket;
+	(void)payload;
+	
+	printf("%s\n", payload);
+	
+	return 0;
+}
+
+PERIODIC_RETURN send_heartbeat(PERIODIC_ARGS) {
+	int r;
+
+	r = network_write(mainsocket_get(), "beat");
+	
+	return r;
+}
+
+void load_functions(void) {
+	command_add("helo", &command_helo);
+	command_add("info", &command_info);
+	command_add("aval", &command_aval);
+	periodic_add(&send_heartbeat);
+}
---
+++ src/example/server.c
@@ -0,0 +1,92 @@
+// Copyright (c) 2016 Brian Barto
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License. See LICENSE for more details.
+
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include "modules/network.h"
+#include "modules/periodic.h"
+#include "modules/command.h"
+#include "modules/socketlist.h"
+#include "modules/sockettime.h"
+#include "modules/mainsocket.h"
+
+#include "blackjack.h"
+
+
+/*
+ * Define functions here
+ */
+ 
+COMMAND_RETURN command_info(COMMAND_ARGS) {
+	(void)socket;
+	(void)payload;
+
+	network_write(socket, "info" "Info about table rules here.");
+	
+	return 0;
+}
+
+COMMAND_RETURN command_join(COMMAND_ARGS) {
+	(void)socket;
+	(void)payload;
+
+	network_write(socket, "aval" "Info about open seats.");
+	
+	return 0;
+}
+
+COMMAND_RETURN command_quit(COMMAND_ARGS) {
+	
+	// Suppress "unused parameter" warning for payload
+	(void)payload;
+	
+	// Close socket
+	close(socket);
+	socketlist_remove(socket);
+	
+	return 0;
+}
+
+COMMAND_RETURN command_beat(COMMAND_ARGS) {
+	printf("Received beat command on socket: %i, payload: %s\n", socket, payload);
+	
+	return 0;
+}
+
+PERIODIC_RETURN periodic(PERIODIC_ARGS) {
+	int i;
+	
+	// Check time for all sockets and close unresponsive ones
+	while ((i = socketlist_get_next()) > 0) {
+		if (i == mainsocket_get())
+			continue;
+
+		// Close socket if idle time elapsed
+		if (sockettime_elapsed(i)) {
+			close(i);
+			socketlist_remove(i);
+		}
+	}
+	
+	return 0;
+}
+
+/*
+ * Load functions here
+ */
+void load_functions(void) {
+	command_add("beat", &command_beat);
+	command_add("info", &command_info);
+	command_add("join", &command_join);
+	command_add("quit", &command_quit);
+	periodic_add(&periodic);
+	
+	// Shuffling all tables;
+	int i;
+	for (i = 0; i < TABLE_MAX; ++i) {
+		blackjack_shuffle_table(i);
+	}
+}
