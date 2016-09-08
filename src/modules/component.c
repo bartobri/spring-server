b0265e4d758acc42ceb04c62d12f931997b91f70
Brian Barto <bartobrian@gmail.com>
2016-09-08 16:14:02 -0400

    Run disconnect function when closing socket.
    	modified:   src/modules/component.c

--- src/modules/component.c
+++ src/modules/component.c
@@ -32,6 +32,8 @@ int write_socket(int s, char *command, char *payload) {
 void close_socket(int s) {
 	close(s);
 	socketlist_remove(s);
+	if (disconnectfunction_exists())
+		disconnectfunction_exec(s);
 }
 
 int main_socket(void) {
