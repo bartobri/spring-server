921475da8296fcb21433c5310604bad93cd8b7d4
Brian Barto <bartobrian@gmail.com>
2016-09-12 09:53:43 -0400

    Document command.c
    	modified:   src/modules/command.c

--- src/modules/command.c
+++ src/modules/command.c
@@ -8,19 +8,36 @@
 #include "config.h"
 #include "modules/command.h"
 
-// Static vars
+// Static Variable Definitions
+
+/*
+ * commands is a static array of commandTbl structures. It stores
+ * command-to-function pairings. This array is traversed to determine
+ * what function to execute for a given command. See the declaration in
+ * command.h for structure member info
+ */
 static struct commandTbl commands[COMMAND_LIMIT];
 
+// Function Definitions
+
+/*
+ * The command_init() function initializes all member values in the
+ * commands structure array to NULL.
+ */
 void command_init(void) {
 	int i;
-	
-	// Init commandfunctions table with all nulls
+
 	for (i = 0; i < COMMAND_LIMIT; ++i) {
 		commands[i].command = NULL;
 		commands[i].functionPtr = NULL;
 	}
 }
 
+/*
+ * The command_add() function adds a given char and function pointer to
+ * the commands structure array in the next available slot determined by
+ * a value of NULL.
+ */
 void command_add(char *command, comFunctionType functionPtr) {
 	int i;
 
@@ -33,6 +50,12 @@ void command_add(char *command, comFunctionType functionPtr) {
 	}
 }
 
+/*
+ * The command_exists() function traverses the commands structure array
+ * looking to match a member command string with the given command string.
+ * A true value is returned if a match is found. A false value is returned
+ * if no match is found.
+ */
 int command_exists(char *command) {
 	int i;
 
@@ -47,6 +70,12 @@ int command_exists(char *command) {
 	return 0;
 }
 
+/*
+ * The command_exec() function traverses the commands structure array
+ * looking to match a member command string with the given command string.
+ * If a match is found, the associated function is executed with the
+ * given socket and payload as parameters.
+ */
 void command_exec(char *command, char *payload, int socket) {
 	int i;
 
