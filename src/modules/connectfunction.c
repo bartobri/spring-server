a9426ae31675eee6a9f6ec287dbb2ed65e500792
Brian Barto <bartobrian@gmail.com>
2016-09-12 15:43:44 -0400

    Document connectfunction.c
    	modified:   src/modules/connectfunction.c

--- src/modules/connectfunction.c
+++ src/modules/connectfunction.c
@@ -6,17 +6,55 @@
 #include <stdlib.h>
 #include "modules/connectfunction.h"
 
-// Static variables
-connectFunctionType connectFunction;
+// MODULE DESCRIPTION
 
+/*
+ * The connectfunction module stores a pointer to a function that is
+ * executed for every new connection established by the server or client.
+ * For the client, there is only one connection ever established, which
+ * is the connection to the server. For the server, there can
+ * potentially be thousands of connections over any given period of time.
+ * 
+ * The connect function is defined and set inside the server and client
+ * module files.
+ * 
+ * The purpose of the function is to initiate a process with the
+ * connecting entity.
+ * 
+ * In most cases, only the client or the server will have a connect
+ * function set. Rarely will they ever both need to have this set. It is
+ * also not necessary for either of them to have this set, as it may be
+ * more sensible for a periodic function to initiate a process after a
+ * particular condition is met.
+ */
+
+// STATIC VARIABLES
+
+/*
+ * Function pointer to the connect function.
+ */
+static connectFunctionType connectFunction;
+
+// FUNCTION DEFINITIONS
+
+/*
+ * Set the connect function pointer to NULL.
+ */
 void connectfunction_init(void) {
 	connectFunction = NULL;
 }
 
+/*
+ * Set the connect function to the given function pointer (functionPtr)
+ */
 void connectfunction_set(connectFunctionType functionPtr) {
 	connectFunction = functionPtr;
 }
 
+/*
+ * Check if the connect function has been set. Return a true value if it
+ * has. Return a false value if it has not.
+ */
 int connectfunction_exists(void) {
 	if (connectFunction != NULL)
 		return 1;
@@ -24,6 +62,10 @@ int connectfunction_exists(void) {
 	return 0;
 }
 
+/*
+ * Execute the connect function if the poiter has been set. Pass as a
+ * parameter the connecting entity's socket assignment.
+ */
 void connectfunction_exec(int socket) {
 	if (connectFunction != NULL)
 		connectFunction(socket);
