abde0878338dc6c631e191edcb927c42747a2af4
Brian Barto <bartobrian@gmail.com>
2016-09-12 16:23:40 -0400

    Document the disconnectfunction module.
    	modified:   src/modules/disconnectfunction.c
    	modified:   src/modules/disconnectfunction.h

--- src/modules/disconnectfunction.c
+++ src/modules/disconnectfunction.c
@@ -6,17 +6,40 @@
 #include <stdlib.h>
 #include "modules/disconnectfunction.h"
 
-// Static variables
+// MODULE DESCRIPTION
+
+/*
+ * The disconnectfunction module manages a pointer to a function referred
+ * to as the "disconnect function". This function is executed by the main
+ * module every time it detected a terminated connection, or terminates
+ * one itself.
+ */
+
+// STATIC VARIABLES
+
+/*
+ * Function pointer to the disconnect function.
+ */
 disconnectFunctionType disconnectFunction;
 
+/*
+ * Set the disconnect function pointer to NULL.
+ */
 void disconnectfunction_init(void) {
 	disconnectFunction = NULL;
 }
 
+/*
+ * Set the disconnect function to the given function pointer (functionPtr)
+ */
 void disconnectfunction_set(disconnectFunctionType functionPtr) {
 	disconnectFunction = functionPtr;
 }
 
+/*
+ * Check if the disconnect function has been set. Return a true value if
+ * it has. Return a false value if it has not.
+ */
 int disconnectfunction_exists(void) {
 	if (disconnectFunction != NULL)
 		return 1;
@@ -24,6 +47,10 @@ int disconnectfunction_exists(void) {
 	return 0;
 }
 
+/*
+ * Execute the connect function if the poiter has been set. Pass as a
+ * parameter the connecting entity's socket assignment.
+ */
 void disconnectfunction_exec(int socket) {
 	if (disconnectFunction != NULL)
 		disconnectFunction(socket);
--- src/modules/disconnectfunction.h
+++ src/modules/disconnectfunction.h
@@ -1,11 +1,27 @@
+// Copyright (c) 2016 Brian Barto
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License. See LICENSE for more details.
+
 #ifndef DISCONNECTFUNCTION_H
 #define DISCONNECTFUNCTION_H 1
 
+/*
+ * These symbolic constants define the parameters and the return value for
+ * the functions executed in response to a disconnection.
+ */
 #define DISCONNECTFUNCTION_ARGS        int socket
 #define DISCONNECTFUNCTION_RETURN      void
 
+/*
+ * Define a data type for a function pointer whos paramaters are
+ * DISCONNECTFUNCTION_ARGS and return value is DISCONNECTFUNCTION_RETURN.
+ */
 typedef DISCONNECTFUNCTION_RETURN (*disconnectFunctionType)(DISCONNECTFUNCTION_ARGS);
 
+/*
+ * Function declarations.
+ */
 void disconnectfunction_init(void);
 void disconnectfunction_set(disconnectFunctionType);
 int disconnectfunction_exists(void);
