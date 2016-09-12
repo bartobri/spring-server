f308551992b8d84b5a4d4f310f40a73a03b060cd
Brian Barto <bartobrian@gmail.com>
2016-09-12 15:46:39 -0400

    Document connectfunction.h
    	modified:   src/modules/connectfunction.h

--- src/modules/connectfunction.h
+++ src/modules/connectfunction.h
@@ -1,11 +1,27 @@
+// Copyright (c) 2016 Brian Barto
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License. See LICENSE for more details.
+
 #ifndef CONNECTFUNCTION_H
 #define CONNECTFUNCTION_H 1
 
+/*
+ * These symbolic constants define the parameters and the return value for
+ * the functions executed in response to a new connection.
+ */
 #define CONNECTFUNCTION_ARGS        int socket
 #define CONNECTFUNCTION_RETURN      void
 
+/*
+ * Define a data type for a function pointer whos paramaters are
+ * CONNECTFUNCTION_ARGS and return value is CONNECTFUNCTION_RETURN.
+ */
 typedef CONNECTFUNCTION_RETURN (*connectFunctionType)(CONNECTFUNCTION_ARGS);
 
+/*
+ * Function declarations.
+ */
 void connectfunction_init(void);
 void connectfunction_set(connectFunctionType);
 int connectfunction_exists(void);
