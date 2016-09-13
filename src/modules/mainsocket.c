787cc164ee28c3df37b56361849c192cb810a269
Brian Barto <bartobrian@gmail.com>
2016-09-13 09:24:15 -0400

    Document the mainsocket module
    	modified:   src/modules/mainsocket.c
    	modified:   src/modules/mainsocket.h

--- src/modules/mainsocket.c
+++ src/modules/mainsocket.c
@@ -3,16 +3,37 @@
 // This program is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License. See LICENSE for more details.
 
+/*
+ * MODULE DESCRIPTION
+ * 
+ * The mainsocket module stores and provides access to the main socket
+ * file descriptor, as an integer value. For the client, this is the
+ * socket used to communicate with the server. For the server, this is
+ * the socket used to listen for new connections.
+ */
+ 
+/*
+ * Static Variables
+ */
 static int mainsocket;
 
+/*
+ * Initialize the mainsocket static variable to NULL (zero).
+ */
 void mainsocket_init(void) {
 	mainsocket = 0;
 }
 
+/*
+ * Store the integer value for the main socket file descriptor.
+ */
 void mainsocket_set(int n) {
 	mainsocket = n;
 }
 
+/*
+ * Return the integer value for the main socket file descriptor.
+ */
 int mainsocket_get(void) {
 	return mainsocket;
 }
--- src/modules/mainsocket.h
+++ src/modules/mainsocket.h
@@ -1,6 +1,14 @@
+// Copyright (c) 2016 Brian Barto
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License. See LICENSE for more details.
+
 #ifndef MAINSOCKET_H
 #define MAINSOCKET_H 1
 
+/*
+ * Function Declarations
+ */
 void mainsocket_init(void);
 void mainsocket_set(int);
 int mainsocket_get(void);
