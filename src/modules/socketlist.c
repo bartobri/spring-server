3bd869cff916f10d457b891e7c6c090b6d300cf9
Brian Barto <bartobrian@gmail.com>
2016-09-13 11:13:30 -0400

    Document the socketlist module
    	modified:   src/modules/socketlist.c
    	modified:   src/modules/socketlist.h

--- src/modules/socketlist.c
+++ src/modules/socketlist.c
@@ -6,23 +6,45 @@
 #include <sys/select.h>
 #include "modules/socketlist.h"
 
-// Static variables
+/*
+ * MODULE DESCRIPTION
+ * 
+ * The socketlist module maintains and provides access to a list of all
+ * active socket file descriptors.
+ */
+
+/*
+ * Static Variables
+ */
 static fd_set active_fd_set;
 static int list_position;
 
+/*
+ * Set all static variable values to zero.
+ */
 void socketlist_init(void) {
 	FD_ZERO(&active_fd_set);
 	list_position = 0;
 }
 
+/*
+ * Add the given socket to the socketlist.
+ */
 void socketlist_add(int socket) {
 	FD_SET(socket, &active_fd_set);
 }
 
+/*
+ * Remove the given socket from the socketlist.
+ */
 void socketlist_remove(int socket) {
 	FD_CLR(socket, &active_fd_set);
 }
 
+/*
+ * Return the next socket value from the socketlist. Return a negative
+ * value when the end of the list is reached.
+ */
 int socketlist_get_next(void) {
 
 	while (++list_position < FD_SETSIZE)
@@ -34,6 +56,9 @@ int socketlist_get_next(void) {
 	return -1;
 }
 
+/*
+ * Reset the list position used for traversing the socketlist.
+ */
 void socketlist_reset_next(void) {
 	list_position = 0;
 }
