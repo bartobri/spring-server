7a2114ca445bde6e1d39daba7f9b93213a22782c
Brian Barto <bartobrian@gmail.com>
2016-09-13 11:29:17 -0400

    Document the termflag module
    	modified:   src/modules/termflag.c
    	modified:   src/modules/termflag.h

--- src/modules/termflag.c
+++ src/modules/termflag.c
@@ -3,17 +3,36 @@
 // This program is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License. See LICENSE for more details.
 
+/*
+ * MODULE DESCRIPTION
+ * 
+ * The termflag module stores and provides access to a simple boolean
+ * flag. This flag can be set from inside a custom function to tell the
+ * main program loop to terminate the program.
+ */
 
+/*
+ * Static Variables
+ */
 static int termflag;
 
+/*
+ * Initialize the flag to a false value.
+ */
 void termflag_init(void) {
 	termflag = 0;
 }
 
+/*
+ * Set the flag to a true value.
+ */
 void termflag_set(void) {
 	termflag = 1;
 }
 
+/*
+ * Return the flag value.
+ */
 int termflag_isset(void) {
 	return termflag;
 } 
--- src/modules/termflag.h
+++ src/modules/termflag.h
@@ -1,6 +1,14 @@
+// Copyright (c) 2016 Brian Barto
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License. See LICENSE for more details.
+
 #ifndef TERMFLAG_H
 #define TERMFLAG_H 1
 
+/*
+ * Function Declarations
+ */
 void termflag_init(void);
 void termflag_set(void);
 int termflag_isset(void);
