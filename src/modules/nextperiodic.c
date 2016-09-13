025a0e780ddadca6b293349cccb05df6a75f5d55
Brian Barto <bartobrian@gmail.com>
2016-09-13 10:34:57 -0400

    Document the nextperiodic module
    	modified:   src/modules/nextperiodic.c
    	modified:   src/modules/nextperiodic.h

--- src/modules/nextperiodic.c
+++ src/modules/nextperiodic.c
@@ -6,17 +6,37 @@
 #include <time.h>
 #include "config.h"
 
-//Static Variables
+/*
+ * MODULE DESCRIPTION
+ * 
+ * The periodictime module manages a timestamp of the last time the
+ * periodic functions were run.
+ */
+
+/*
+ * Static Variables
+ */
 static int periodictime;
 
+/*
+ * Initialize periodictime to the current time.
+ */
 void nextperiodic_init(void) {
 	periodictime = (int)time(NULL);
 }
 
+/*
+ * Set periodictime to the current time.
+ */
 void nextperiodic_reset(void) {
 	periodictime = (int)time(NULL);
 }
 
+/*
+ * Check if the number of seconds configured for PERIODIC_SECONDS has
+ * elapsed since the last time periodictime was set. Return a true value
+ * if it has. Return a false value if it has not.
+ */
 int nextperiodic_elapsed(void) {
 	if (periodictime <= time(NULL) - PERIODIC_SECONDS)
 		return 1;
--- src/modules/nextperiodic.h
+++ src/modules/nextperiodic.h
@@ -1,6 +1,14 @@
+// Copyright (c) 2016 Brian Barto
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License. See LICENSE for more details.
+
 #ifndef NEXTPERIODIC_H
 #define NEXTPERIODIC_H 1
 
+/*
+ * Function Declarations
+ */
 void nextperiodic_init(void);
 void nextperiodic_reset(void);
 int nextperiodic_elapsed(void);
