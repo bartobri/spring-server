e536a67e2012e7e8b07c6efd1f235e83b1707bcd
Brian Barto <bartobrian@gmail.com>
2017-04-01 12:30:52 -0400

    Update license info
    	modified:   LICENSE
    	modified:   src/client.c
    	modified:   src/config.h
    	modified:   src/example/client.c
    	modified:   src/example/server.c
    	modified:   src/modules/command.c
    	modified:   src/modules/command.h
    	modified:   src/modules/component.c
    	modified:   src/modules/component.h
    	modified:   src/modules/connectfunction.c
    	modified:   src/modules/connectfunction.h
    	modified:   src/modules/disconnectfunction.c
    	modified:   src/modules/disconnectfunction.h
    	modified:   src/modules/inputcommand.c
    	modified:   src/modules/inputcommand.h
    	modified:   src/modules/inputpayload.c
    	modified:   src/modules/inputpayload.h
    	modified:   src/modules/log.c
    	modified:   src/modules/log.h
    	modified:   src/modules/main.c
    	modified:   src/modules/main.h
    	modified:   src/modules/mainsocket.c
    	modified:   src/modules/mainsocket.h
    	modified:   src/modules/network.c
    	modified:   src/modules/network.h
    	modified:   src/modules/nextperiodic.c
    	modified:   src/modules/nextperiodic.h
    	modified:   src/modules/periodic.c
    	modified:   src/modules/periodic.h
    	modified:   src/modules/readlist.c
    	modified:   src/modules/readlist.h
    	modified:   src/modules/socketlist.c
    	modified:   src/modules/socketlist.h
    	modified:   src/modules/sockettime.c
    	modified:   src/modules/sockettime.h
    	modified:   src/modules/termflag.c
    	modified:   src/modules/termflag.h
    	modified:   src/server.c

--- src/config.h
+++ src/config.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2017 Brian Barto
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License. See LICENSE for more details.
+
 #ifndef CONFIG_H
 #define CONFIG_H 1
 
--- src/modules/main.h
+++ src/modules/main.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2017 Brian Barto
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License. See LICENSE for more details.
+
 #ifndef MAIN_H
 #define MAIN_H 1
 
